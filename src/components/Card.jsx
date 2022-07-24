e2395f1172743b20c383b7540835d2b4e7fe1601
Emre Boz <emrento@gmail.com>
2022-07-24 21:01:46 +0300

    ornek ürünl listeleme

---
+++ src/components/Card.jsx
@@ -0,0 +1,15 @@
+function Cart({baslik,gorselUrl}) {
+    
+ 
+
+
+    return (
+        <>
+        <h2>{baslik}</h2>
+        <img src={gorselUrl}></img>
+        </>
+
+    )
+}
+
+export default App;
